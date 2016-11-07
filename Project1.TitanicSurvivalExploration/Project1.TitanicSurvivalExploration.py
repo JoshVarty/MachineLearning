5e6ac47257425954c852f092f82c82571e74a6f3
Josh Varty <joshvarty@gmail.com>
2016-11-07 01:14:59 -0800

    My first submission attempt for Project 1

---
+++ Project1.TitanicSurvivalExploration/Project1.TitanicSurvivalExploration.py
@@ -0,0 +1,129 @@
+# Import libraries necessary for this project
+import numpy as np
+import pandas as pd
+from IPython.display import display # Allows the use of display() for DataFrames
+
+# Import supplementary visualizations code visuals.py
+import visuals as vs
+
+# Pretty display for notebooks
+# %matplotlib inline
+
+# Load the dataset
+in_file = 'titanic_data.csv'
+full_data = pd.read_csv(in_file)
+
+# Print the first few entries of the RMS Titanic data
+display(full_data.head())
+
+# Store the 'Survived' feature in a new variable and remove it from the dataset
+outcomes = full_data['Survived']
+data = full_data.drop('Survived', axis = 1)
+
+# Show the new dataset with 'Survived' removed
+display(data.head())
+
+def accuracy_score(truth, prediction):
+    """ Returns accuracy score for input truth and predictions. """
+    
+    # Ensure that the number of predictions matches number of outcomes
+    if len(truth) == len(prediction): 
+        
+        # Calculate and return the accuracy as a percent
+        return "Predictions have an accuracy of {:.2f}%.".format((truth == prediction).mean()*100)
+    
+    else:
+        return "Number of predictions does not match number of outcomes!"
+    
+# Test the 'accuracy_score' function
+predictions = pd.Series(np.ones(5, dtype = int))
+print accuracy_score(outcomes[:5], predictions)
+
+
+def predictions_0(data):
+    """ Model with no features. Always predicts a passenger did not survive. """
+
+    predictions = []
+    for _, passenger in data.iterrows():
+        
+        # Predict the survival of 'passenger'
+        predictions.append(0)
+    
+    # Return our predictions
+    return pd.Series(predictions)
+
+# Make the predictions
+predictions = predictions_0(data)
+print accuracy_score(outcomes, predictions)
+
+
+#vs.survival_stats(data, outcomes, 'Sex')
+
+def predictions_1(data):
+    """ Model with one feature: 
+            - Predict a passenger survived if they are female. """
+    
+    predictions = []
+    for _, passenger in data.iterrows():
+        if passenger.Sex == "female":
+            predictions.append(True)
+        else:
+            predictions.append(False)
+    
+    # Return our predictions
+    return pd.Series(predictions)
+
+# Make the predictions
+predictions = predictions_1(data)
+
+#vs.survival_stats(data, outcomes, 'Age', ["Sex == 'male'"])
+
+def predictions_2(data):
+    """ Model with two features: 
+            - Predict a passenger survived if they are female.
+            - Predict a passenger survived if they are male and younger than 10. """
+    
+    predictions = []
+    for _, passenger in data.iterrows():
+        if passenger.Sex == "female":
+            predictions.append(True)
+        elif passenger.Sex == "male" and passenger.Age < 10:
+            predictions.append(True)
+        else:
+            predictions.append(False)
+    
+    # Return our predictions
+    return pd.Series(predictions)
+
+# Make the predictions
+predictions = predictions_2(data)
+
+print accuracy_score(outcomes, predictions)
+
+
+def predictions_3(data):
+    """ Model with multiple features. Makes a prediction with an accuracy of at least 80%. """
+    
+    predictions = []
+    for _, passenger in data.iterrows():
+        if passenger.Age < 10:
+            predictions.append(True)
+        elif passenger.SibSp > 4:
+            predictions.append(False)
+        elif passenger.Sex == "female":
+            predictions.append(True)
+        elif passenger.Fare > 200:
+            predictions.append(True)
+        elif passenger.Pclass == 3:
+            predictions.append(False)
+        else:
+            predictions.append(False)
+        # Remove the 'pass' statement below 
+        # and write your prediction conditions here
+    
+    # Return our predictions
+    return pd.Series(predictions)
+
+# Make the predictions
+predictions = predictions_3(data)
+print accuracy_score(outcomes, predictions)
