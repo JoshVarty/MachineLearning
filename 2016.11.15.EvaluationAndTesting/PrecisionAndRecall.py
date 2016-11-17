9e5503d75f16c8f6486d6e629cb2ca6063151cd1
Josh Varty <joshvarty@gmail.com>
2016-11-16 23:36:12 -0800

    Update precision and recall

--- 2016.11.15.EvaluationAndTesting/PrecisionAndRecall.py
+++ 2016.11.15.EvaluationAndTesting/PrecisionAndRecall.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 
 # Load the dataset
-X = pd.read_csv('titanic_data.csv')
+X = pd.read_csv('C:\\Users\\Josh\\Documents\\GitHub\\MachineLearning\\Project1.TitanicSurvivalExploration\\titanic_data.csv')
 
 X = X._get_numeric_data()
 y = X['Survived']
@@ -21,18 +21,20 @@ from sklearn.naive_bayes import GaussianNB
 # TODO: split the data into training and testing sets,
 # using the standard settings for train_test_split.
 # Then, train and test the classifiers with your newly split data instead of X and y.
+from sklearn import cross_validation
+labels_train, labels_test, features_train, features_test = cross_validation.train_test_split(X, y, test_size=0.4, random_state=0) 
 
 clf1 = DecisionTreeClassifier()
-clf1.fit(X, y)
-print "Decision Tree recall: {:.2f} and precision: {:.2f}".format(recall(y,clf1.predict(X)),precision(y,clf1.predict(X)))
+clf1.fit(labels_train, features_train)
+print "Decision Tree recall: {:.2f} and precision: {:.2f}".format(recall(features_test, clf1.predict(labels_test)), precision(features_test, clf1.predict(labels_test)))
 
 clf2 = GaussianNB()
-clf2.fit(X, y)
-print "GaussianNB recall: {:.2f} and precision: {:.2f}".format(recall(y,clf2.predict(X)),precision(y,clf2.predict(X)))
+clf2.fit(labels_train, features_train)
+print "GaussianNB recall: {:.2f} and precision: {:.2f}".format(recall(features_test ,clf2.predict(labels_test)), precision(features_test, clf2.predict(labels_test)))
 
 results = {
-  "Naive Bayes Recall": 0,
-  "Naive Bayes Precision": 0,
-  "Decision Tree Recall": 0,
-  "Decision Tree Precision": 0
+  "Naive Bayes Recall": recall(features_test ,clf2.predict(labels_test)),
+  "Naive Bayes Precision": precision(features_test, clf2.predict(labels_test)),
+  "Decision Tree Recall": recall(features_test, clf1.predict(labels_test)),
+  "Decision Tree Precision": precision(features_test, clf1.predict(labels_test)))
 }
