c4f95ae203f36d5580fa7f85f2c468f8dcf92eed
Josh Varty <joshvarty@gmail.com>
2016-11-17 00:37:36 -0800

    Added MeanSquaredError quiz

---
+++ 2016.11.15.EvaluationAndTesting/MeanSquaredError.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+
+# Load the dataset
+from sklearn.datasets import load_linnerud
+
+linnerud_data = load_linnerud()
+X = linnerud_data.data
+y = linnerud_data.target
+
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_squared_error as mse
+from sklearn.linear_model import LinearRegression
+
+# TODO: split the data into training and testing sets,
+# using the standard settings for train_test_split.
+# Then, train and test the classifiers with your newly split data instead of X and y.
+from sklearn import cross_validation
+labels_train, labels_test, features_train, features_test = cross_validation.train_test_split(X, y, test_size=0.4, random_state=0) 
+
+
+reg1 = DecisionTreeRegressor()
+reg1.fit(labels_train, features_train)
+print "Decision Tree mean absolute error: {:.2f}".format(mse(features_test, reg1.predict(labels_test)))
+
+reg2 = LinearRegression()
+reg2.fit(labels_train, features_train)
+print "Linear regression mean absolute error: {:.2f}".format(mse(features_test, reg2.predict(labels_test)))
+
+results = {
+ "Linear Regression": mse(features_test, reg2.predict(labels_test)),
+ "Decision Tree": mse(features_test, reg1.predict(labels_test))
+}
