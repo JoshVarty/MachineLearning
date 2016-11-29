5ddc250b0c6d9c5d5cde7eb647b8b18dc1026c33
Josh Varty <joshvarty@gmail.com>
2016-11-29 01:28:36 -0800

    Added polynomial regression quiz

---
+++ 2016.11.25.SupervisedLearning/PolynomialRegression.py
@@ -0,0 +1,17 @@
+#
+#	Polynomial Regression
+#
+#	In this exercise we will examine more complex models of test grades as a function of 
+#	sleep using numpy.polyfit to determine a good relationship and incorporating more data.
+#
+#
+#   at the end, store the coefficients of the polynomial you found in coeffs
+#
+
+import numpy as np
+
+sleep = [5,6,7,8,10,12,16]
+scores = [65,51,75,75,86,80,0]
+
+
+coeffs = []
