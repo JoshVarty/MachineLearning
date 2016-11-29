052eb39a8e9fa020cba9d6ab76358c64bd7e8e21
Josh Varty <joshvarty@gmail.com>
2016-11-28 23:13:25 -0800

    Added solution to quiz

--- 2016.11.25.SupervisedLearning/studentRegression.py
+++ 2016.11.25.SupervisedLearning/studentRegression.py
@@ -1,9 +1,10 @@
 def studentReg(ages_train, net_worths_train):
     ### import the sklearn regression module, create, and train your regression
+    from sklearn import linear_model
     ### name your regression reg
+    reg = linear_model.LinearRegression()
     
     ### your code goes here!
-    
-    
+    reg.fit(ages_train, net_worths_train)
     
     return reg
