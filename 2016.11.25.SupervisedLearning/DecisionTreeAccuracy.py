124620b882d5d492f835327022a486480a579308
Josh Varty <joshvarty@gmail.com>
2016-11-30 00:29:49 -0800

    Compute accuracies for min_samples_split 2 and 50

--- 2016.11.25.SupervisedLearning/DecisionTreeAccuracy.py
+++ 2016.11.25.SupervisedLearning/DecisionTreeAccuracy.py
@@ -1,6 +1,7 @@
 import sys
 from class_vis import prettyPicture
 from prep_terrain_data import makeTerrainData
+from sklearn.tree import DecisionTreeClassifier
 
 import numpy as np
 import pylab as pl
@@ -8,24 +9,18 @@ import pylab as pl
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
 
-#################################################################################
-
-
-########################## DECISION TREE #################################
-
-
-
-#### your code goes here
-
-from sklearn.tree import DecisionTreeClassifier
 ### your code goes here--should return a trained decision tree classifer
 
-clf = DecisionTreeClassifier()
+clf = DecisionTreeClassifier(min_samples_split = 2)
 clf.fit(features_train, labels_train)
 
+acc_min_samples_split_2 = clf.score(features_test, labels_test)
 
-acc = clf.score(features_test, labels_test)
+clf = DecisionTreeClassifier(min_samples_split = 50)
+clf.fit(features_train, labels_train)
+
+acc_min_samples_split_50 = clf.score(features_test, labels_test)
 
 def submitAccuracies():
-  return {"acc":round(acc,3)}
-
+  return {"acc_min_samples_split_2":round(acc_min_samples_split_2,3),
+          "acc_min_samples_split_50":round(acc_min_samples_split_50,3)}
