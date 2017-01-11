c9f9dc344d36361a2d6077fd35c993954f0e989c
Josh Varty <joshvarty@gmail.com>
2017-01-10 22:11:16 -0800

    Encoding of variables

--- Project3.FindingDonors/Project3.FindingDonors.py
+++ Project3.FindingDonors/Project3.FindingDonors.py
@@ -36,3 +36,45 @@ print "Total number of records: {}".format(n_records)
 print "Individuals making more than $50,000: {}".format(n_greater_50k)
 print "Individuals making at most $50,000: {}".format(n_at_most_50k)
 print "Percentage of individuals making more than $50,000: {:.2f}%".format(greater_percent)
+
+
+# Split the data into features and target label
+income_raw = data['income']
+features_raw = data.drop('income', axis = 1)
+
+# Visualize skewed continuous features of original data
+vs.distribution(data)
+
+# Log-transform the skewed features
+skewed = ['capital-gain', 'capital-loss']
+features_raw[skewed] = data[skewed].apply(lambda x: np.log(x + 1))
+
+# Visualize the new log distributions
+vs.distribution(features_raw, transformed = True)
+
+
+# Import sklearn.preprocessing.StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+
+# Initialize a scaler, then apply it to the features
+scaler = MinMaxScaler()
+numerical = ['age', 'education-num', 'capital-gain', 'capital-loss', 'hours-per-week']
+features_raw[numerical] = scaler.fit_transform(data[numerical])
+
+# Show an example of a record with scaling applied
+display(features_raw.head(n = 1))
+
+
+import pandas
+# One-hot encode the 'features_raw' data using pandas.get_dummies()
+features = pandas.get_dummies(features_raw) 
+
+# Encode the 'income_raw' data to numerical values
+income = pandas.get_dummies(income_raw)
+
+# Print the number of features after one-hot encoding
+encoded = list(features.columns)
+print "{} total features after one-hot encoding.".format(len(encoded))
+
+# Uncomment the following line to see the encoded feature names
+#print encoded
