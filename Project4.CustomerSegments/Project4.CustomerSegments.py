9c53ec67868abbc38bdb62cfa8677f76db48fb4a
Josh Varty <joshvarty@gmail.com>
2017-01-22 15:03:35 -0800

    Answer to Question1

---
+++ Project4.CustomerSegments/Project4.CustomerSegments.py
@@ -0,0 +1,31 @@
+# Import libraries necessary for this project
+import numpy as np
+import pandas as pd
+from IPython.display import display # Allows the use of display() for DataFrames
+
+# Import supplementary visualizations code visuals.py
+import visuals as vs
+
+# Pretty display for notebooks
+#%matplotlib inline
+
+# Load the wholesale customers dataset
+try:
+    data = pd.read_csv("customers.csv")
+    data.drop(['Region', 'Channel'], axis = 1, inplace = True)
+    print "Wholesale customers dataset has {} samples with {} features each.".format(*data.shape)
+except:
+    print "Dataset could not be loaded. Is the dataset missing?"
+
+
+# Display a description of the dataset
+display(data.describe())
+
+
+# Select three indices of your choice you wish to sample from the dataset
+indices = [23,35,85]
+
+# Create a DataFrame of the chosen samples
+samples = pd.DataFrame(data.loc[indices], columns = data.keys()).reset_index(drop = True)
+print "Chosen samples of wholesale customers dataset:"
+display(samples)
