2274c5ffd1dc840bfd27d59c283cd78e1b87c057
Josh Varty <joshvarty@gmail.com>
2017-02-09 00:03:26 -0800

    Answer Problem 2

--- 2017.02.05.DeepLearning/1_notmnist.py
+++ 2017.02.05.DeepLearning/1_notmnist.py
@@ -108,3 +108,81 @@ for i in range(0,3):
     print(image_path)
     display(image)
 
+
+
+
+#Now let's load the data in a more manageable format. Since, you might not be able to fit it all in memory, 
+#we'll load each class into a separate dataset, store them on disk and curate them independently. 
+#Later we'll merge them into a single dataset of manageable size.
+
+#We'll convert the entire dataset into a 3D array (image index, x, y) of floating point values, 
+#normalized to have approximately zero mean and standard deviation ~0.5 to make training easier down the road.
+
+#A few images might not be readable, we'll just skip them.
+
+image_size = 28  # Pixel width and height.
+pixel_depth = 255.0  # Number of levels per pixel.
+
+def load_letter(folder, min_num_images):
+  """Load the data for a single letter label."""
+  image_files = os.listdir(folder)
+  dataset = np.ndarray(shape=(len(image_files), image_size, image_size),
+                         dtype=np.float32)
+  print(folder)
+  num_images = 0
+  for image in image_files:
+    image_file = os.path.join(folder, image)
+    try:
+      image_data = (ndimage.imread(image_file).astype(float) - 
+                    pixel_depth / 2) / pixel_depth
+      if image_data.shape != (image_size, image_size):
+        raise Exception('Unexpected image shape: %s' % str(image_data.shape))
+      dataset[num_images, :, :] = image_data
+      num_images = num_images + 1
+    except IOError as e:
+      print('Could not read:', image_file, ':', e, '- it\'s ok, skipping.')
+    
+  dataset = dataset[0:num_images, :, :]
+  if num_images < min_num_images:
+    raise Exception('Many fewer images than expected: %d < %d' %
+                    (num_images, min_num_images))
+    
+  print('Full dataset tensor:', dataset.shape)
+  print('Mean:', np.mean(dataset))
+  print('Standard deviation:', np.std(dataset))
+  return dataset
+        
+def maybe_pickle(data_folders, min_num_images_per_class, force=False):
+  dataset_names = []
+  for folder in data_folders:
+    set_filename = folder + '.pickle'
+    dataset_names.append(set_filename)
+    if os.path.exists(set_filename) and not force:
+      # You may override by setting force=True.
+      print('%s already present - Skipping pickling.' % set_filename)
+    else:
+      print('Pickling %s.' % set_filename)
+      dataset = load_letter(folder, min_num_images_per_class)
+      try:
+        with open(set_filename, 'wb') as f:
+          pickle.dump(dataset, f, pickle.HIGHEST_PROTOCOL)
+      except Exception as e:
+        print('Unable to save data to', set_filename, ':', e)
+  
+  return dataset_names
+
+train_datasets = maybe_pickle(train_folders, 45000)
+test_datasets = maybe_pickle(test_folders, 1800)
+
+
+#Problem 2
+#Let's verify that the data still looks good. Displaying a sample of the labels and images from the ndarray. 
+#Hint: you can use matplotlib.pyplot.
+
+pickle_file = train_datasets[0]  # index 0 should be all As, 1 = all Bs, etc.
+with open(pickle_file, 'rb') as f:
+    letter_set = pickle.load(f)  # unpickle
+    sample_idx = np.random.randint(len(letter_set))  # pick a random image index
+    sample_image = letter_set[sample_idx, :, :]  # extract a 2D slice
+    plt.figure()
+    plt.imshow(sample_image)  # display it
