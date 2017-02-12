a53c2531cb0ce66d6f248840dd71903f9c1d920e
Josh Varty <joshvarty@gmail.com>
2017-02-12 15:26:50 -0800

    Introduce 1 hidden relu layer for 89.6% accuracy

--- 2017.02.05.DeepLearning/2_fullyconnected.py
+++ 2017.02.05.DeepLearning/2_fullyconnected.py
@@ -51,42 +51,42 @@ print('Test set', test_dataset.shape, test_labels.shape)
 # Subset the training data for faster turnaround.
 train_subset = 10000
 
-graph = tf.Graph()
-with graph.as_default():
+#graph = tf.Graph()
+#with graph.as_default():
 
-  # Input data.
-  # Load the training, validation and test data into constants that are
-  # attached to the graph.
-  tf_train_dataset = tf.constant(train_dataset[:train_subset, :])
-  tf_train_labels = tf.constant(train_labels[:train_subset])
-  tf_valid_dataset = tf.constant(valid_dataset)
-  tf_test_dataset = tf.constant(test_dataset)
+#  # Input data.
+#  # Load the training, validation and test data into constants that are
+#  # attached to the graph.
+#  tf_train_dataset = tf.constant(train_dataset[:train_subset, :])
+#  tf_train_labels = tf.constant(train_labels[:train_subset])
+#  tf_valid_dataset = tf.constant(valid_dataset)
+#  tf_test_dataset = tf.constant(test_dataset)
   
-  # Variables.
-  # These are the parameters that we are going to be training. The weight
-  # matrix will be initialized using random values following a (truncated)
-  # normal distribution. The biases get initialized to zero.
-  weights = tf.Variable(tf.truncated_normal([image_size * image_size, num_labels]))
-  biases = tf.Variable(tf.zeros([num_labels]))
+#  # Variables.
+#  # These are the parameters that we are going to be training. The weight
+#  # matrix will be initialized using random values following a (truncated)
+#  # normal distribution. The biases get initialized to zero.
+#  weights = tf.Variable(tf.truncated_normal([image_size * image_size, num_labels]))
+#  biases = tf.Variable(tf.zeros([num_labels]))
   
-  # Training computation.
-  # We multiply the inputs with the weight matrix, and add biases. We compute
-  # the softmax and cross-entropy (it's one operation in TensorFlow, because
-  # it's very common, and it can be optimized). We take the average of this
-  # cross-entropy across all training examples: that's our loss.
-  logits = tf.matmul(tf_train_dataset, weights) + biases
-  loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels, logits=logits))
+#  # Training computation.
+#  # We multiply the inputs with the weight matrix, and add biases. We compute
+#  # the softmax and cross-entropy (it's one operation in TensorFlow, because
+#  # it's very common, and it can be optimized). We take the average of this
+#  # cross-entropy across all training examples: that's our loss.
+#  logits = tf.matmul(tf_train_dataset, weights) + biases
+#  loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels, logits=logits))
   
-  # Optimizer.
-  # We are going to find the minimum of this loss using gradient descent.
-  optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+#  # Optimizer.
+#  # We are going to find the minimum of this loss using gradient descent.
+#  optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
   
-  # Predictions for the training, validation, and test data.
-  # These are not part of training, but merely here so that we can report
-  # accuracy figures as we train.
-  train_prediction = tf.nn.softmax(logits)
-  valid_prediction = tf.nn.softmax(tf.matmul(tf_valid_dataset, weights) + biases)
-  test_prediction = tf.nn.softmax(tf.matmul(tf_test_dataset, weights) + biases)
+#  # Predictions for the training, validation, and test data.
+#  # These are not part of training, but merely here so that we can report
+#  # accuracy figures as we train.
+#  train_prediction = tf.nn.softmax(logits)
+#  valid_prediction = tf.nn.softmax(tf.matmul(tf_valid_dataset, weights) + biases)
+#  test_prediction = tf.nn.softmax(tf.matmul(tf_test_dataset, weights) + biases)
 
 
 num_steps = 801
@@ -94,32 +94,68 @@ num_steps = 801
 def accuracy(predictions, labels):
   return 100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1)) / predictions.shape[0]
 
-with tf.Session(graph=graph) as session:
-  # This is a one-time operation which ensures the parameters get initialized as
-  # we described in the graph: random weights for the matrix, zeros for the
-  # biases. 
-  tf.global_variables_initializer().run()
-  print('Initialized')
-  for step in range(num_steps):
-    _, l, predictions = session.run([optimizer, loss, train_prediction])
-    if (step % 100 == 0):
-      print('Loss at step %d: %f' % (step, l))
-      print('Training accuracy: %.1f%%' % accuracy(
-        predictions, train_labels[:train_subset, :]))
-      # Calling .eval() on valid_prediction is basically like calling run(), but
-      # just to get that one numpy array. Note that it recomputes all its graph
-      # dependencies.
-      print('Validation accuracy: %.1f%%' % accuracy(
-        valid_prediction.eval(), valid_labels))
+#with tf.Session(graph=graph) as session:
+#  # This is a one-time operation which ensures the parameters get initialized as
+#  # we described in the graph: random weights for the matrix, zeros for the
+#  # biases. 
+#  tf.global_variables_initializer().run()
+#  print('Initialized')
+#  for step in range(num_steps):
+#    _, l, predictions = session.run([optimizer, loss, train_prediction])
+#    if (step % 100 == 0):
+#      print('Loss at step %d: %f' % (step, l))
+#      print('Training accuracy: %.1f%%' % accuracy(
+#        predictions, train_labels[:train_subset, :]))
+#      # Calling .eval() on valid_prediction is basically like calling run(), but
+#      # just to get that one numpy array. Note that it recomputes all its graph
+#      # dependencies.
+#      print('Validation accuracy: %.1f%%' % accuracy(
+#        valid_prediction.eval(), valid_labels))
 
-  print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
-
-
+#  print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
 
 
 
 
-#Use stochastic gradient descent (we use a random subset to train on instead of the full set)
+
+
+##Use stochastic gradient descent (we use a random subset to train on instead of the full set)
+#batch_size = 128
+
+#graph = tf.Graph()
+#with graph.as_default():
+#  # Input data. For the training data, we use a placeholder that will be fed
+#  # at run time with a training minibatch.
+#  tf_train_dataset = tf.placeholder(tf.float32, shape=(batch_size, image_size * image_size))
+#  tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
+#  tf_valid_dataset = tf.constant(valid_dataset)
+#  tf_test_dataset = tf.constant(test_dataset)
+  
+#  # Variables.
+#  weights = tf.Variable(tf.truncated_normal([image_size * image_size, num_labels]))
+#  biases = tf.Variable(tf.zeros([num_labels]))
+  
+#  # Training computation.
+#  logits = tf.matmul(tf_train_dataset, weights) + biases
+#  loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels, logits=logits))
+  
+#  # Optimizer.
+#  optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+  
+#  # Predictions for the training, validation, and test data.
+#  train_prediction = tf.nn.softmax(logits)
+#  valid_prediction = tf.nn.softmax(tf.matmul(tf_valid_dataset, weights) + biases)
+#  test_prediction = tf.nn.softmax(tf.matmul(tf_test_dataset, weights) + biases)
+
+
+
+
+
+
+
+#Problem
+#Turn the logistic regression example with SGD into a 1-hidden layer neural network with rectified linear units nn.relu() and 1024 hidden nodes. 
+#This model should improve your validation / test accuracy.
 batch_size = 128
 
 graph = tf.Graph()
@@ -130,13 +166,18 @@ with graph.as_default():
   tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
   tf_valid_dataset = tf.constant(valid_dataset)
   tf_test_dataset = tf.constant(test_dataset)
-  
+
+  hidden_layer_size = 1024
+  hidden_weights = tf.Variable(tf.truncated_normal([image_size * image_size, hidden_layer_size]))
+  hidden_biases = tf.Variable(tf.zeros([hidden_layer_size]))
+  hidden_layer = tf.nn.relu(tf.matmul(tf_train_dataset, hidden_weights) + hidden_biases)
+
   # Variables.
-  weights = tf.Variable(tf.truncated_normal([image_size * image_size, num_labels]))
+  weights = tf.Variable(tf.truncated_normal([hidden_layer_size, num_labels]))
   biases = tf.Variable(tf.zeros([num_labels]))
   
   # Training computation.
-  logits = tf.matmul(tf_train_dataset, weights) + biases
+  logits = tf.matmul(hidden_layer, weights) + biases
   loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels, logits=logits))
   
   # Optimizer.
@@ -144,9 +185,14 @@ with graph.as_default():
   
   # Predictions for the training, validation, and test data.
   train_prediction = tf.nn.softmax(logits)
-  valid_prediction = tf.nn.softmax(tf.matmul(tf_valid_dataset, weights) + biases)
-  test_prediction = tf.nn.softmax(tf.matmul(tf_test_dataset, weights) + biases)
 
+  valid_hidden = tf.nn.relu(tf.matmul(tf_valid_dataset, hidden_weights) + hidden_biases)
+  valid_logits = tf.matmul(valid_hidden, weights) + biases
+  valid_prediction = tf.nn.softmax(valid_logits)
+
+  test_hidden = tf.nn.relu(tf.matmul(tf_test_dataset, hidden_weights) + hidden_biases)
+  test_logits = tf.matmul(test_hidden, weights) + biases
+  test_prediction = tf.nn.softmax(test_logits)
 
 
 num_steps = 3001
