66dbf7cffed5608b5a0eacc963c96aa46be44e65
Josh Varty <joshvarty@gmail.com>
2017-02-12 17:50:12 -0800

    L2 Regularization for a one layer net

--- 2017.02.05.DeepLearning/3_regularization.py
+++ 2017.02.05.DeepLearning/3_regularization.py
@@ -105,7 +105,85 @@ def LogisticRegression(l2_weight):
 
       return accuracy(test_prediction.eval(), test_labels)
 
-result = LogisticRegression(0.0);
-print("0.000 " + str(result) + "%"); # 86.21%
-result = LogisticRegression(0.001);
-print("0.001 " + str(result) + "%"); # 89.16%
+#result = LogisticRegression(0.0);
+#print("0.000 " + str(result) + "%"); # 86.21%
+#result = LogisticRegression(0.001);
+#print("0.001 " + str(result) + "%"); # 89.16%
+
+
+def OneHiddenLayer(l2_weight):
+    batch_size = 128
+
+    graph = tf.Graph()
+    with graph.as_default():
+      # Input data. For the training data, we use a placeholder that will be fed
+      # at run time with a training minibatch.
+      tf_train_dataset = tf.placeholder(tf.float32, shape=(batch_size, image_size * image_size))
+      tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
+      tf_valid_dataset = tf.constant(valid_dataset)
+      tf_test_dataset = tf.constant(test_dataset)
+
+      hidden_layer_size = 1024
+      hidden_weights = tf.Variable(tf.truncated_normal([image_size * image_size, hidden_layer_size]))
+      hidden_biases = tf.Variable(tf.zeros([hidden_layer_size]))
+      hidden_layer = tf.nn.relu(tf.matmul(tf_train_dataset, hidden_weights) + hidden_biases)
+
+      # Variables.
+      weights = tf.Variable(tf.truncated_normal([hidden_layer_size, num_labels]))
+      biases = tf.Variable(tf.zeros([num_labels]))
+      
+      # Training computation.
+      logits = tf.matmul(hidden_layer, weights) + biases
+      loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels, logits=logits)) 
+      + l2_weight * tf.nn.l2_loss(weights) 
+      + l2_weight * tf.nn.l2_loss(hidden_weights)
+      
+      # Optimizer.
+      optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+      
+      # Predictions for the training, validation, and test data.
+      train_prediction = tf.nn.softmax(logits)
+
+      valid_hidden = tf.nn.relu(tf.matmul(tf_valid_dataset, hidden_weights) + hidden_biases)
+      valid_logits = tf.matmul(valid_hidden, weights) + biases
+      valid_prediction = tf.nn.softmax(valid_logits)
+
+      test_hidden = tf.nn.relu(tf.matmul(tf_test_dataset, hidden_weights) + hidden_biases)
+      test_logits = tf.matmul(test_hidden, weights) + biases
+      test_prediction = tf.nn.softmax(test_logits)
+
+
+    num_steps = 3001
+
+    with tf.Session(graph=graph) as session:
+      tf.global_variables_initializer().run()
+      print("Initialized")
+      for step in range(num_steps):
+        # Pick an offset within the training data, which has been randomized.
+        # Note: we could use better randomization across epochs.
+        offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+        # Generate a minibatch.
+        batch_data = train_dataset[offset:(offset + batch_size), :]
+        batch_labels = train_labels[offset:(offset + batch_size), :]
+        # Prepare a dictionary telling the session where to feed the minibatch.
+        # The key of the dictionary is the placeholder node of the graph to be fed,
+        # and the value is the numpy array to feed to it.
+        feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
+        _, l, predictions = session.run([optimizer, loss, train_prediction], feed_dict=feed_dict)
+        #if (step % 500 == 0):
+          #print("Minibatch loss at step %d: %f" % (step, l))
+          #print("Minibatch accuracy: %.1f%%" % accuracy(predictions, batch_labels))
+          #print("Validation accuracy: %.1f%%" % accuracy(valid_prediction.eval(), valid_labels))
+
+      return accuracy(test_prediction.eval(), test_labels)
+
+result = OneHiddenLayer(0.0);
+print("0.000 " + str(result) + "%"); 
+result = OneHiddenLayer(0.001);
+print("0.001 " + str(result) + "%"); 
+result = OneHiddenLayer(0.010);
+print("0.010 " + str(result) + "%"); 
+result = OneHiddenLayer(0.100);
+print("0.100 " + str(result) + "%"); 
+result = OneHiddenLayer(0.5);
+print("0.500 " + str(result) + "%"); 
