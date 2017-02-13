f78a8279e4aac5762feab7466e2a3bcaa851e9b5
Josh Varty <joshvarty@gmail.com>
2017-02-12 16:48:52 -0800

    Include Assignment3

---
+++ 2017.02.05.DeepLearning/3_regularization.py
@@ -0,0 +1,55 @@
+# These are all the modules we'll be using later. Make sure you can import them
+# before proceeding further.
+from __future__ import print_function
+import numpy as np
+import tensorflow as tf
+from six.moves import cPickle as pickle
+import os
+
+data_root = 'C:\\data\\' # Change me to store data elsewhere
+pickle_file = 'notMNIST.pickle'
+dest_file_path = os.path.join(data_root, pickle_file)
+
+with open(dest_file_path, 'rb') as f:
+  save = pickle.load(f)
+  train_dataset = save['train_dataset']
+  train_labels = save['train_labels']
+  valid_dataset = save['valid_dataset']
+  valid_labels = save['valid_labels']
+  test_dataset = save['test_dataset']
+  test_labels = save['test_labels']
+  del save  # hint to help gc free up memory
+  print('Training set', train_dataset.shape, train_labels.shape)
+  print('Validation set', valid_dataset.shape, valid_labels.shape)
+  print('Test set', test_dataset.shape, test_labels.shape)
+
+  
+
+image_size = 28
+num_labels = 10
+
+def reformat(dataset, labels):
+  dataset = dataset.reshape((-1, image_size * image_size)).astype(np.float32)
+  # Map 1 to [0.0, 1.0, 0.0 ...], 2 to [0.0, 0.0, 1.0 ...]
+  labels = (np.arange(num_labels) == labels[:,None]).astype(np.float32)
+  return dataset, labels
+
+train_dataset, train_labels = reformat(train_dataset, train_labels)
+valid_dataset, valid_labels = reformat(valid_dataset, valid_labels)
+test_dataset, test_labels = reformat(test_dataset, test_labels)
+
+print('Training set', train_dataset.shape, train_labels.shape)
+print('Validation set', valid_dataset.shape, valid_labels.shape)
+print('Test set', test_dataset.shape, test_labels.shape)
+
+def accuracy(predictions, labels):
+  return 100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1)) / predictions.shape[0]
+
+
+#Introduce and tune L2 regularization for both logistic and neural network models. 
+#Remember that L2 amounts to adding a penalty on the norm of the weights to the loss. 
+#In TensorFlow, you can compute the L2 loss for a tensor t using nn.l2_loss(t). 
+#The right amount of regularization should improve your validation / test accuracy.
+
+
+
