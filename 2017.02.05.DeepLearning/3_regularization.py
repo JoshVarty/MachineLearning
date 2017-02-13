f0c3494fc0fa6355f0a14998a637fd69aa38dfe0
Josh Varty <joshvarty@gmail.com>
2017-02-12 17:36:17 -0800

    L2 regularization for logistic regression

--- 2017.02.05.DeepLearning/3_regularization.py
+++ 2017.02.05.DeepLearning/3_regularization.py
@@ -51,5 +51,61 @@ def accuracy(predictions, labels):
 #In TensorFlow, you can compute the L2 loss for a tensor t using nn.l2_loss(t). 
 #The right amount of regularization should improve your validation / test accuracy.
 
+def LogisticRegression(l2_weight):
+    batch_size = 128
+
+    graph = tf.Graph()
+    with graph.as_default():
+
+      # Input data. For the training data, we use a placeholder that will be fed
+      # at run time with a training minibatch.
+      tf_train_dataset = tf.placeholder(tf.float32,shape=(batch_size, image_size * image_size))
+      tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
+      tf_valid_dataset = tf.constant(valid_dataset)
+      tf_test_dataset = tf.constant(test_dataset)
+      
+      # Variables.
+      weights = tf.Variable(tf.truncated_normal([image_size * image_size, num_labels]))
+      biases = tf.Variable(tf.zeros([num_labels]))
+      
+      # Training computation.
+      logits = tf.matmul(tf_train_dataset, weights) + biases
+      loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels, logits=logits)) + l2_weight * tf.nn.l2_loss(weights)
+      
+      # Optimizer.
+      optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+      
+      # Predictions for the training, validation, and test data.
+      train_prediction = tf.nn.softmax(logits)
+      valid_prediction = tf.nn.softmax(tf.matmul(tf_valid_dataset, weights) + biases)
+      test_prediction = tf.nn.softmax(tf.matmul(tf_test_dataset, weights) + biases)
 
 
+      num_steps = 3001
+
+    with tf.Session(graph=graph) as session:
+      tf.global_variables_initializer().run()
+      print("Initialized")
+      for step in range(num_steps):
+        # Pick an offset within the training data, which has been randomized.
+        # Note: we could use better randomization across epochs.
+        offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+        # Generate a minibatch.
+        batch_data = train_dataset[offset:(offset + batch_size), :]
+        batch_labels = train_labels[offset:(offset + batch_size), :]
+        # Prepare a dictionary telling the session where to feed the minibatch.
+        # The key of the dictionary is the placeholder node of the graph to be fed,
+        # and the value is the numpy array to feed to it.
+        feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
+        _, l, predictions = session.run([optimizer, loss, train_prediction], feed_dict=feed_dict)
+        if (step % 500 == 0):
+          print("Minibatch loss at step %d: %f" % (step, l))
+          print("Minibatch accuracy: %.1f%%" % accuracy(predictions, batch_labels))
+          print("Validation accuracy: %.1f%%" % accuracy(valid_prediction.eval(), valid_labels))
+
+      return accuracy(test_prediction.eval(), test_labels)
+
+result = LogisticRegression(0.0);
+print("0.000 " + str(result) + "%"); # 86.21%
+result = LogisticRegression(0.001);
+print("0.001 " + str(result) + "%"); # 89.16%
