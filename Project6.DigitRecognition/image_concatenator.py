414fe6e2092dd572a409d94afda49e4bc977e5b3
Josh Varty <joshvarty@gmail.com>
2017-02-15 23:51:14 -0800

    Save data to disk

--- Project6.DigitRecognition/image_concatenator.py
+++ Project6.DigitRecognition/image_concatenator.py
@@ -28,34 +28,67 @@ with open(dest_file_path, 'rb') as f:
 
 
 from IPython.display import Image
-def generate(dataset):
+def generate(dataset, labels):
     newDataSet = []
+    newLabels = []
     numImages = len(dataset)
     minImageLength = 1
     maxImageLength = 5
-    for image in dataset:
+    for index, image in enumerate(dataset):
         newImage = np.zeros((28, 28 * 5))
+        newLabel = []
         #How long will the new image be?
         imageLength = random.randint(minImageLength, maxImageLength)
 
-        print(imageLength)
+        #print(imageLength)
         for i in range(0, imageLength):
-            randomImage = random.choice(dataset)
+            randomInt = random.randint(0, len(dataset) - 1)
+            randomImage = dataset[randomInt]
+            newLabel.append(labels[randomInt])
             #Splice randomeImage into new image
             newImage[:, (i) * 28 : (i+1) * 28] = randomImage[:,:]
-            print("From Image: " + str(i))
+            #print("From Image: " + str(i))
 
         for i in range(imageLength, 5):
             blankImage = np.zeros((28,28))
             blankImage.fill(0)
-
+            newLabel.append(-1)
             newImage[:, (i) * 28 : (i+1) * 28] = blankImage
-            print("AppendZerios: " + str(i))
+            #print("AppendZerios: " + str(i))
 
-        plt.figure()
-        plt.imshow(newImage)
+        newDataSet.append(newImage)
+        newLabels.append(newLabel)
 
+        if index % 1000 == 0:
+            print("Index: " + str(index));
+            #plt.figure()
+            #plt.imshow(newImage)
+            #print(newLabel)
 
+    return newDataSet, newLabels
 
-generate(test_dataset[1:3])
+newTrainData, newTrainLabels = generate(train_dataset, train_labels)
+newValidData, newValidLabels = generate(valid_dataset, valid_dataset)
+newTestData, newTestLabels = generate(test_dataset, test_labels)
 
+#Finally, let's save the data for later reuse:
+pickle_file = os.path.join(data_root, 'five_digit_notMNIST.pickle')
+
+try:
+  f = open(pickle_file, 'wb')
+  save = {
+    'train_dataset': newTrainData,
+    'train_labels': newTrainLabels,
+    'valid_dataset': newValidData,
+    'valid_labels': newValidLabels,
+    'test_dataset': newTestData,
+    'test_labels': newTestLabels,
+    }
+  pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
+  f.close()
+except Exception as e:
+  print('Unable to save data to', pickle_file, ':', e)
+  raise
+
+statinfo = os.stat(pickle_file)
+print('Compressed pickle size:', statinfo.st_size)
