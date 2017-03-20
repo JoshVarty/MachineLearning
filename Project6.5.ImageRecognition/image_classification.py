838ef212af081626c34268981032c2a1ad399da8
Josh Varty <joshvarty@gmail.com>
2017-03-19 18:14:38 -0700

    Add normalization code

--- Project6.5.ImageRecognition/image_classification.py
+++ Project6.5.ImageRecognition/image_classification.py
@@ -2,12 +2,13 @@
 DON'T MODIFY ANYTHING IN THIS CELL THAT IS BELOW THIS LINE
 """
 from urllib.request import urlretrieve
-from os.path import isfile, isdir
+from os.path import isfile, isdir, join
 from tqdm import tqdm
 import problem_unittests as tests
 import tarfile
 
 cifar10_dataset_folder_path = 'C:\\data\\cifar-10-batches-py'
+cifar10_dataset_file_path = 'C:\\data\\cifar-10-python.tar.gz'
 
 class DLProgress(tqdm):
     last_block = 0
@@ -17,7 +18,7 @@ class DLProgress(tqdm):
         self.update((block_num - self.last_block) * block_size)
         self.last_block = block_num
 
-if not isfile('cifar-10-python.tar.gz'):
+if not isfile(cifar10_dataset_file_path):
     with DLProgress(unit='B', unit_scale=True, miniters=1, desc='CIFAR-10 Dataset') as pbar:
         urlretrieve(
             'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz',
@@ -25,8 +26,40 @@ if not isfile('cifar-10-python.tar.gz'):
             pbar.hook)
 
 if not isdir(cifar10_dataset_folder_path):
-    with tarfile.open('cifar-10-python.tar.gz') as tar:
+    with tarfile.open(cifar10_dataset_file_path) as tar:
         tar.extractall()
         tar.close()
 
 tests.test_folder_path(cifar10_dataset_folder_path)
+
+
+
+#%matplotlib inline
+#%config InlineBackend.figure_format = 'retina'
+import helper
+import numpy as np
+
+# Explore the dataset
+batch_id = 1
+sample_id = 5
+#helper.display_stats(cifar10_dataset_folder_path, batch_id, sample_id)
+
+
+
+def normalize(x):
+    """
+    Normalize a list of sample image data in the range of 0 to 1
+    : x: List of image data.  The image shape is (32, 32, 3)
+    : return: Numpy array of normalize data
+    """
+
+    newImages = np.empty_like(x)
+    newImages = x / 255
+    return np.array(newImages)
+
+
+
+"""
+DON'T MODIFY ANYTHING IN THIS CELL THAT IS BELOW THIS LINE
+"""
+tests.test_normalize(normalize)
