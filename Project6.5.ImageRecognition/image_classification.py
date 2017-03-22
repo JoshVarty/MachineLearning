df52cb6f973a7db637ed8d53a993c364b618e4ef
Josh Varty <joshvarty@gmail.com>
2017-03-22 00:18:38 -0700

    Need valid features now

--- Project6.5.ImageRecognition/image_classification.py
+++ Project6.5.ImageRecognition/image_classification.py
@@ -93,7 +93,7 @@ import problem_unittests as tests
 import helper
 
 # Load the Preprocessed Validation data
-#valid_features, valid_labels = pickle.load(open('preprocess_validation.p', mode='rb'))
+valid_features, valid_labels = pickle.load(open('preprocess_validation.p', mode='rb'))
 
 
 
